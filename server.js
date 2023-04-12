a4bd09484cfcb75b7205b187e0165096c52edd98
mingyue21 <myyyyywang@gmail.com>
2023-04-11 19:47:56 -0700

    configure mongodb

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
+
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Node.js server is running on port ${port}`));
