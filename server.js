11de43f9ac5049c896df93cb697bae058c86c641
mingyue21 <myyyyywang@gmail.com>
2023-04-13 13:22:32 -0700

    configure cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from 'cors';
 const app = express();
+app.use(cors());
+app.use(express.json());
 import dotenv from 'dotenv';
 dotenv.config();
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
-app.use(express.json());
 import userRoute from './routes/userRoute.js';
 
 app.use('/api/users', userRoute);
