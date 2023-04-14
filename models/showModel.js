5947ac3228c45db1d9bc6cbc42d171c2f4b6e489
jinxin0731 <jin.xin4@northeastern.edu>
2023-04-14 14:01:06 -0700

    11 12 13

---
+++ models/showModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const showSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    date : {
+        type: Date,
+        required: true
+    },
+    time : {
+        type: String,
+        required: true
+    },
+    movie : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'movies',
+        required: true
+    },
+    ticketPrice : {
+        type: Number,
+        required: true
+    },
+    totalSeats : {
+        type: Number,
+        required: true
+    },
+    bookedSeats : {
+        type: Array,
+        default: []
+    },
+    theatre : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'theatres',
+        required: true
+    },
+} , { timestamps: true });
+
+export default mongoose.model('shows', showSchema);
--- routes/theatresRoute.js
+++ routes/theatresRoute.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 const router = Router();
 import Theatre from '../models/theatreModel.js';
+import Show from "../models/showModel.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 // add a theatre
 router.post('/add-theatre', async (req, res) => {
@@ -86,6 +88,120 @@ router.delete('/delete-theatre', async (req, res) => {
     }
 });
 
+// add show
+router.post("/add-show", authMiddleware, async (req, res) => {
+    try {
+        const newShow = new Show(req.body);
+        await newShow.save();
+        res.send({
+            success: true,
+            message: "Show added successfully",
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+
+// get all shows by theatre
+router.post("/get-all-shows-by-theatre", authMiddleware, async (req, res) => {
+    try {
+        const shows = await Show.find({ theatre: req.body.theatreId })
+            .populate("movie")
+            .sort({
+                createdAt: -1,
+            });
+
+        res.send({
+            success: true,
+            message: "Shows fetched successfully",
+            data: shows,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+
+// delete show
+router.post("/delete-show", authMiddleware, async (req, res) => {
+    try {
+        await Show.findByIdAndDelete(req.body.showId);
+        res.send({
+            success: true,
+            message: "Show deleted successfully",
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+
+// get all unique theatres which have shows of a movie
+router.post("/get-all-theatres-by-movie", authMiddleware, async (req, res) => {
+    try {
+        const { movie, date } = req.body;
+
+        // find all shows of a movie
+        const shows = await Show.find({ movie, date })
+            .populate("theatre")
+            .sort({ createdAt: -1 });
+
+        // get all unique theatres
+        let uniqueTheatres = [];
+        shows.forEach((show) => {
+            const theatre = uniqueTheatres.find(
+                (theatre) => theatre._id === show.theatre._id
+            );
+
+            if (!theatre) {
+                const showsForThisTheatre = shows.filter(
+                    (showObj) => showObj.theatre._id === show.theatre._id
+                );
+                uniqueTheatres.push({
+                    ...show.theatre._doc,
+                    shows: showsForThisTheatre,
+                });
+            }
+        });
+
+        res.send({
+            success: true,
+            message: "Theatres fetched successfully",
+            data: uniqueTheatres,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+
+// get show by id
+router.post("/get-show-by-id", authMiddleware, async (req, res) => {
+    try {
+        const show = await Show.findById(req.body.showId)
+            .populate("movie")
+            .populate("theatre");
+        res.send({
+            success: true,
+            message: "Show fetched successfully",
+            data: show,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
 
 export default router;
 
