17cb21ef53b701ffa1f4a2a902d58386a6fa05a2
mingyue21 <myyyyywang@gmail.com>
2023-04-14 22:22:50 -0700

    set token key into environment variable

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const authMiddleware = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const secretKey = 'moviemart';
-        const decoded = jwt.verify(token, secretKey);
+        const decoded = jwt.verify(token, process.env.jwt_secret);
         req.body.userId = decoded.userId;
         next();
     } catch (err) {
